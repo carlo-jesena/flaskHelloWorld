806248ba5a43b927f403a9609176c62df940393e
Your Namecarlo <carlo.jesena@gmail.com>
2017-01-24 23:39:44 -0800

    connected with mongodb, able to create

--- hello_world.py
+++ hello_world.py
@@ -1,19 +1,31 @@
 from flask import Flask
 from os import environ
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+
+app.config['MONGOA_DBNAME'] = 'pythontest'
+app.config['MONGO_URI'] = 'mongodb://python:python@ds129469.mlab.com:29469/pythontest'
+
+mongo = PyMongo(app)
+
+@app.route('/add')
+def add():
+    user = mongo.db.users
+    user.insert({'name' : 'Carlo'})
+    return 'Added User!'
 
 @app.route("/")
 @app.route("/hello")
 def say_hi():
     return "Hello World!"
 
-# @app.route("/hello/<name>")
-# def say_person(name):
-#     return "Hello {}!".format(name.title())
-
 @app.route("/hello/<name>")
-def hello_person(name):
+def say_person(name):
+    return "Hello {}!".format(name.title())
+
+@app.route("/hello/<names>")
+def hello_person(names):
     html = """
         <h1>
             Hello {}!
@@ -25,8 +37,9 @@ def hello_person(name):
     """
     return html.format(name.title())
 
-@app.route('/users/')
-
+@app.route('/users/<username>', methods=['POST'])
+def new_user(username):
+    return
 
 if __name__ == "__main__":
     app.run(debug=True)
