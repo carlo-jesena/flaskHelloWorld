72b5120c93db9ae109109fc46d6624bfc819f89a
Your Namecarlo <carlo.jesena@gmail.com>
2017-01-24 16:34:20 -0800

    added dynamic route

--- hello_world.py
+++ hello_world.py
@@ -4,13 +4,30 @@ from os import environ
 app = Flask(__name__)
 
 @app.route("/")
-# @app.route("/hello")
+@app.route("/hello")
 def say_hi():
     return "Hello World!"
 
+# @app.route("/hello/<name>")
+# def say_person(name):
+#     return "Hello {}!".format(name.title())
+
+@app.route("/hello/<name>")
+def hello_person(name):
+    html = """
+        <h1>
+            Hello {}!
+        </h1>
+        <p>
+            Here's a picture of a kitten.  Awww...
+        </p>
+        <img src="http://placekitten.com/g/200/300">
+    """
+    return html.format(name.title())
+
+
+
 if __name__ == "__main__":
     app.run(debug=True)
-
-
     # app.run(host=environ['IP'],
     #         port=int(environ['PORT']))
